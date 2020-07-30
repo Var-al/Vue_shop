a0fe84bce4bc3250c5cf2adf85966cbf809a1171
Var_al <var_al@163.com>
2020-07-30 10:30:09 +0800

    完成了分类参数的开发

--- src/router.js
+++ src/router.js
@@ -7,6 +7,7 @@ import Users from './components/user/Users.vue'
 import Rights from './components/power/Rights.vue'
 import Roles from './components/power/Roles.vue'
 import Cate from './components/goods/Cate.vue'
+import Params from './components/goods/Params.vue'
 // 导入字体图标
 import './assets/fonts/iconfont.css'
 // 导入全局样式表
@@ -27,7 +28,8 @@ const router = new VueRouter({
         { path: '/users', component: Users },
         { path: '/rights', component: Rights},
         { path: '/roles', component: Roles},
-        { path: '/categories', component: Cate}
+        { path: '/categories', component: Cate},
+        {path: '/params', component: Params}
 
       ]
     },
